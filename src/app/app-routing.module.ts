c615718e540acbea9145aed077b01f3fc02d4dbd
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-09-14 22:11:26 -0300

    Criando sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'about'
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./componentes/menu/menu.module').then((m) => m.MenuModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
