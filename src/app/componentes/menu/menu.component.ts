3fc13313fb89c4f0c757e00b4aedd43a7b5d55ec
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-09-17 21:48:32 -0300

    Adicionando menu hamburguer

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -7,10 +7,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.menuCollapse();
   }
 
+  menuCollapse() {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    console.log('passei aqui');
+    if (sidebarToggle) {
+      // Uncomment Below to persist sidebar toggle between refreshes
+      if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        document.body.classList.toggle('sb-sidenav-toggled');
+        console.log('passei aqui');
+      }
+      console.log('passei aqui');
+      sidebarToggle.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.classList.toggle('sb-sidenav-toggled');
+        localStorage.setItem(
+          'sb|sidebar-toggle',
+          String(document.body.classList.contains('sb-sidenav-toggled'))
+        );
+      });
+    }
+  }
 }
